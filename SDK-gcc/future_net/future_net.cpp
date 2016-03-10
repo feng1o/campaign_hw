dfcb3ae4e2b2ab253067b8fd0a5ae84ba2588e4a
feng1o <liufeng_mail@126.com>
2016-03-10 21:27:17 +0800

    xxx

--- SDK-gcc/future_net/future_net.cpp
+++ SDK-gcc/future_net/future_net.cpp
@@ -2,7 +2,10 @@
 #include "route.h"
 #include "lib_io.h"
 #include "lib_time.h"
+
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 #include "iostream"
 int main(int argc, char *argv[])
 {
@@ -15,12 +18,21 @@ int main(int argc, char *argv[])
     char *graph_file = argv[1];
     edge_num = read_file(graph, 5000, graph_file);
     printf("\n .........edge_num = %d\n", edge_num);
+	for(int i=0;i<1;i++) // edge_num
+		{
+            std::cout << "graph[" << i <<"]" << graph[0] << std::endl;
+            for(int j =0 ; j<8; j++)
+			std::cout<<"graph[" << i << "][" << j << "]" << graph[i][j] <<std::endl;
+		}
     std::cout<<".............................\n\n";
     char *condition_file = argv[2];
     condition_num = read_file(&condition, 1, condition_file);
-	for(int i=0;i<condition_num;i++)
+
+	unsigned int condition_len = strlen(condition);
+    std::cout << "............"  << condition_len << std::endl;
+	for(int i=0; i<=condition_len ;i++)
 		{
-			std::cout<<" -path-"<<condition[i] <<std::endl;
+			std::cout<<" condition[" << i << "]:" << condition[i] <<std::endl;
 		}
     search_route(graph, edge_num, condition);
 
