2d6b7eb6832924caa12ae97301c806af69813ae9
feng1o <liufeng_mail@126.com>
2016-03-18 23:34:02 +0800

    croslist

--- SDK-gcc/future_net/my_dealData.h
+++ SDK-gcc/future_net/my_dealData.h
@@ -38,6 +38,50 @@ typedef struct
 
 
 
+/* crosslist */
+typedef struct str_crossListArc   crossListArc;
+typedef struct str_crossListArc*  pcrossListArc;
+typedef struct str_crossList      crossListHead;
+typedef struct str_crossList*      pCrossListHead;
+struct str_crossListArc
+{
+	const u_short 		   startNode;
+	const u_short		   endNode;
+	u_short			       Cost;
+	const int              edgeNumber;
+	pcrossListArc	       down;
+	pcrossListArc          right;
+	//bool		           known;
+};
+struct str_crosslistHeadRow {
+	bool          is_key;	//是否关键点
+	bool	      known;		//是否走过
+	u_short       layer;		//层数
+	u_short       outDgree;	//出度
+	pcrossListArc next;
+};
+struct str_crosslistHeadColumn {
+	bool		   is_key;	//是否关键点
+	bool		   known;		//是否走过
+	u_short		   layer;		//层数
+	u_short		   inDgree;	//ru度
+	pcrossListArc  next;
+};
+
+struct str_crossList
+{
+	struct str_crosslistHeadRow*	head_row;
+	struct str_crosslistHeadColumn* head_colunm;
+	u_short 						nodeCount;
+	int								edgeCount;
+};
+
+pCrossListHead creatCrossList(int edgenum);
+u_short insertCrossList(pCrossListHead crossList, signed short nodeNum, const u_short startNode, \
+	const u_short endNode, u_short cost, const int edgenum);
+void printCrossList(pCrossListHead crosslist, int num);
+/* crosslist */
+
 
 void conditionCharToUshort(u_short  vertexCondition[600], const char *condition);
 u_short graphCharToCrosList(tyCrossList* crosslist, char *graph[5000], int edgeNum);
---
+++ SDK-gcc/future_net/route(LIU-PC--liu--2016-03-10-23,10,53).h
@@ -0,0 +1,34 @@
+#ifndef __ROUTE_H__
+#define __ROUTE_H__
+
+void search_route(char *graph[5000], int edge_num, char *condition);
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
