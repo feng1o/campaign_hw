59771e799fbf7fe4eacc62bfb860e7aa4165993d
feng1o <liufeng_mail@126.com>
2016-03-19 13:07:59 +0800

    deleteduplate

---
+++ SDK-gcc/future_net/order.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+echo "one key complie "
+echo
+sh /home/huawei-code-craft/SDK-gcc/batch32.sh
+echo 
+    /home/huawei-code-craft/SDK-gcc/bin/future_net  /home/huawei-code-craft/test-case/case1/topo.csv  /home/huawei-code-craft/test-case/case1/demand.csv  /home/huawei-code-craft/test-case/case1/result.csv 
+
